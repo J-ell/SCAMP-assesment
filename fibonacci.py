7238e266f03cf6cfde6e1a534682facd12706c3e
J-ell <64992471+J-ell@users.noreply.github.com>
2020-05-09 03:03:23 +0100

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,1 +1,12 @@
+# Program to output a given number of fibonacci sequence
+def fibbo(digits):
+    if(digits <= 1):
+        return digits
+    else:
+        return (fibbo(digits-1) + fibbo(digits-2))
 
+digits = int(input("Enter number of sequence:"))
+
+print("Fibonacci sequence :")
+for num in range(digits):
+    print(fibbo(num))
